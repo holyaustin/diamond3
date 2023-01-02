458d42d70ea7a4b93ceb8423c89e10bda3b00c24
holyaustin <holyaustin@yahoo.com>
2023-01-02 11:34:41 +0100

    initial commit

---
+++ config.js
@@ -0,0 +1,7 @@
+
+  export const DiamondCutFacet = "0xe453FA562F1921E47e5C39Ccb3A0Bd8d2bC6f194"
+  export const Diamond = "0x0b9D56517A5a2b46f97881A7EB2147D744615acf"
+  export const DiamondInit = "0x412a119a744d7e5792ca4583C8C7fC1BbA833796"
+  export const DiamondCut = "[object Object],[object Object]"
+  export const DiamondTxn = "0x6453f7cbe843b4400c1533dd19ede723f5def142090eef99a86c16b09a7a95d5"
+  
---
+++ hardhat.config.js
@@ -0,0 +1,45 @@
+
+/* global ethers task */
+require('@nomiclabs/hardhat-waffle')
+require('dotenv').config();
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task('accounts', 'Prints the list of accounts', async () => {
+  const accounts = await ethers.getSigners()
+
+  for (const account of accounts) {
+    console.log(account.address)
+  }
+})
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+   
+    mumbai: {
+      url: 'https://rpc.ankr.com/polygon_mumbai', 
+      accounts: [process.env.PRIVATE_KEY],
+    },
+
+  },
+  solidity: {
+    version: "0.8.17",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+};
+
